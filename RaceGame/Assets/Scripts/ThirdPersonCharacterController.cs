a5062b052a859ae35421d0fcf1916431fb13a694
Jose Pardo <jose.pardo@ue-germany.de>
2021-04-13 12:15:49 -0500

    Added the script and flow machines to the new scene

---
+++ RaceGame/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour {
+
+    public float Speed;
+
+    void Update() {
+        PlayerMovement();
+    }
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 PlayerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        transform.Translate(PlayerMovement, Space.Self);
+    }
+}
