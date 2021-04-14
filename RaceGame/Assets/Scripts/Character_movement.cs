ea3a1ab296aada0cc305ae772fecbdf8badd56a1
Sunny <sunaina.miller@ue-germany.de>
2021-04-14 13:30:51 +0200

    Aparrently 'UnityEditor.UI' does not exist so changed it to 'UnityEditor.UIElements' in the character movement script

--- RaceGame/Assets/Scripts/Character_movement.cs
+++ RaceGame/Assets/Scripts/Character_movement.cs
@@ -1,19 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.UI;
+using UnityEditor.UIElements;
 
 public class Character_movement : MonoBehaviour
 {
     public float speedX = 2.0f;
     float yaw = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         yaw += speedX * Input.GetAxis("Mouse X");
