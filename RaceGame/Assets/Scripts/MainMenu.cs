dc2ec21f02418ebe6cf7481972a50eeb5031c122
Sunny <sunaina.miller@ue-germany.de>
2021-05-05 10:41:45 +0200

    Started adding multiplayer aspects & imported Photon into the Unity project; seems like the others will have to do it on their devices as well, as it does not translate over Git, just like Bolt

--- RaceGame/Assets/Scripts/MainMenu.cs
+++ RaceGame/Assets/Scripts/MainMenu.cs
@@ -2,10 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
-   
-    public void PlayGame ()
+
+    //serialize = transforming object state/data into format that can be stored
+    [SerializeField] private string VersionName = "0.1";
+    [SerializeField] private GameObject UsernameMenu;
+    [SerializeField] private GameObject ConnectPanel;
+
+    [SerializeField] private InputField UsernameInput; //entering username
+    [SerializeField] private InputField CreateGameInput;
+    [SerializeField] private InputField JoinGameInput;
+
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings(VersionName); //connecting to photon servers(?)
+    }
+
+    private void Start()
+    {
+        UsernameMenu.SetActive(true);
+    }
+
+    private void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("Connected");
+    }
+
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
