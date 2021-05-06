635910a6320702efa8379ab91a5b6619a88b25ea
Sunny <sunaina.miller@ue-germany.de>
2021-05-06 10:37:42 +0200

    Username can be inputted, username required to play

--- RaceGame/Assets/Scripts/MainMenu.cs
+++ RaceGame/Assets/Scripts/MainMenu.cs
@@ -15,6 +15,8 @@ public class MainMenu : MonoBehaviour {
     [SerializeField] private InputField CreateGameInput;
     [SerializeField] private InputField JoinGameInput;
 
+    [SerializeField] private GameObject PlayButtons;
+
     private void Awake()
     {
         PhotonNetwork.ConnectUsingSettings(VersionName); //connecting to photon servers(?)
@@ -25,12 +27,33 @@ public class MainMenu : MonoBehaviour {
         UsernameMenu.SetActive(true);
     }
 
+    //connecting to photon servers (?)
     private void OnConnectedToMaster()
     {
         PhotonNetwork.JoinLobby(TypedLobby.Default);
         Debug.Log("Connected");
     }
 
+    public void ChangeUserNameInput()
+    {   //username input required to play game
+        if(UsernameInput.text.Length >= 1) //if minimum 1 letter for username
+        {
+            PlayButtons.SetActive(true);
+        }
+        else
+        {
+            PlayButtons.SetActive(false);
+        }
+    }
+
+    //setting username & starting game
+    public void SetUserName()
+    {
+        UsernameMenu.SetActive(false);
+        PhotonNetwork.playerName = UsernameInput.text;
+    }
+
+    //Samuel's part (starting & quitting game)
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
